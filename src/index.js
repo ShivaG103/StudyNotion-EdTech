d169ff005e88c7498db711f89678c540c3a18210
Shivam Gupta <88878330+ShivaG103@users.noreply.github.com>
2024-02-19 14:49:55 +0530

    favicon location changes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { Toaster } from "react-hot-toast";
 
 if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
   // Browser is in dark mode, update favicon to dark mode
-  document.getElementById('favicon').href = '/favicon.ico';
+  document.getElementById('favicon').href = '/faviconLight.ico';
 }
 
 const store = configureStore({
